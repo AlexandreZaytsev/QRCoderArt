4029a9bf3bdd55b51c62865aa60e07978fed5abf
zaytsev <zaytsev@cad.ru>
2021-07-01 16:07:43 +0300

    fix

--- QRCoderReflection.cs
+++ QRCoderReflection.cs
@@ -114,7 +114,10 @@ namespace RicQRCoderArt
                         break;
                     default:
                         if (param.ParameterType.IsEnum) 
-                        { 
+                        {
+ //                           Dictionary<string, ConstructorInfo> pEnum = (from parN in param.ParameterType.GetEnumNames()
+ //                                                                        from parV in param.ParameterType.GetEnumValues()
+ //                                                                      select new { parN, parV }).ToDictionary(k => k.parN, v => v.parV);
                         }
                         if (param.ParameterType.IsClass) 
                         {
