8cad1e81e0bcb30f8c087528a7cd25668247fa3f
zaytsev <zaytsev@cad.ru>
2021-07-02 14:50:29 +0300

    fix

--- QRCoderReflection.cs
+++ QRCoderReflection.cs
@@ -18,13 +18,6 @@ namespace RicQRCoderArt
     }
     public class QRCoderReflection : IDisposable
     {
-        /*
-        https://blog.rc21net.ru/рефлексия-отражение-reflection-в-c-sharp/
-        https://johnlnelson.com/tag/assembly-gettypes/
-        https://johnlnelson.com/2014/06/17/system-reflection-working-with-the-type-class/
-        https://forum.unity.com/threads/c-reflection-refuses-to-give-me-private-fields.587506/
-        https://docs.microsoft.com/en-us/dotnet/api/system.type.getfields?view=netframework-4.7.2
-        */
         private Type tRef;
 
         public QRCoderReflection(string name)
@@ -41,7 +34,6 @@ namespace RicQRCoderArt
         //получить словарь конструкторов для выпадающего списка
         public Dictionary<string, ConstructorInfo> GetConstructor(MemberInfo mi)
         {
-            //https://metanit.com/sharp/tutorial/14.2.php
             return (from ctor in ((Type)mi).GetConstructors()
                     select new { V = string.Join(", ", ctor.GetParameters().Select(pr => pr.Name)), ctor }).ToDictionary(k => k.V, v => v.ctor);
         }
@@ -49,9 +41,6 @@ namespace RicQRCoderArt
         //инициализировать конструктор и выполнить метод по умолчанию
         public string GetPayloadString(ConstructorInfo ctor, ArrayList ctorP)
         {
-            //https://www.nookery.ru/understand-with-reflection/
-            //https://www.thebestcsharpprogrammerintheworld.com/2017/01/29/using-linq-with-reflection-in-c/
-            //            object ctorObj = ctor.Invoke(new object[] { "MyWiFi-SSID", "MyWiFi-Pass", PayloadGenerator.WiFi.Authentication.WPA, false });
             object ctorObj = ctor.Invoke(ctorP.Cast<object>().ToArray());
             MethodInfo baseMethod = ctor.ReflectedType.GetMethod("ToString");
             return baseMethod.Invoke(ctorObj, null).ToString();
@@ -60,26 +49,7 @@ namespace RicQRCoderArt
         //получить member по имени
         public MemberInfo GetMemberByName(string baseName)
         {
-
             MemberInfo mmbers = tRef.GetMember(baseName).First();
-            //         var c = GetConstructor(mmbers);
-
-            /*
-                        //            https://www.nookery.ru/understand-with-reflection/
-                        Type mi = Asm.GetType("QRCoder.PayloadGenerator");
-                        MemberInfo[] members = mi.GetMember(baseName);
-                        MethodInfo method = mi.GetMethod(baseName);
-                        ParameterInfo[] parameters = method.GetParameters();
-                        // Выводим некоторые характеристики каждого из параметров. 
-                        foreach (ParameterInfo parameter in parameters)
-                        {
-                            Console.WriteLine("Имя параметра: {0}", parameter.Name);
-                            Console.WriteLine("Позиция в методе: {0}", parameter.Position);
-                            Console.WriteLine("Тип параметра: {0}", parameter.ParameterType);
-                        }
-
-                        */
-            //           MethodInfo mt=null;//= etAsm..ge
             return mmbers;
         }
         //получить имена классов payload
@@ -192,92 +162,18 @@ namespace RicQRCoderArt
 
 
 /*
-            //https://blog.rc21net.ru/рефлексия-отражение-reflection-в-c-sharp/
-            //Assembly asm = System.Reflection.Assembly.Load("QRCoder");
-            Assembly asm = System.Reflection.Assembly.ReflectionOnlyLoad("QRCoder");
-            //            Module[] mAsm = asm.GetModules(true);
-            //            Type[] tAsm = asm.GetTypes();
-            Type[] etAsm = asm.GetExportedTypes();
-            //https://johnlnelson.com/tag/assembly-gettypes/
-            //robust code always checks for null FIRST
-            if (etAsm != null && etAsm.Length > 0)
-            {
-                //                System.Text.StringBuilder sb = new System.Text.StringBuilder(); //we'll create a StringBuilder for our formatted output
-                foreach (Type type in etAsm)                //foreach (MemberInfo minf in tAsm)
-                {
-                    if ((type.BaseType.Name) == "Payload" && (!type.IsAbstract))                    //работаем только с метаданными родитель = "Payload"  
-                    {
-                        this.cbPayload.Items.Add(type.Name);
+        https://blog.rc21net.ru/рефлексия-отражение-reflection-в-c-sharp/
 
+        https://johnlnelson.com/tag/assembly-gettypes/
+        https://johnlnelson.com/2014/06/17/system-reflection-working-with-the-type-class/
 
+        https://forum.unity.com/threads/c-reflection-refuses-to-give-me-private-fields.587506/
 
-                        //Display(0, "Member: {0}", type.Name);
-                        //get the methods in the type
-                        //https://johnlnelson.com/2014/06/17/system-reflection-working-with-the-type-class/
-                        //https://forum.unity.com/threads/c-reflection-refuses-to-give-me-private-fields.587506/
-                        //https://docs.microsoft.com/en-us/dotnet/api/system.type.getfields?view=netframework-4.7.2
-                        MethodInfo[] methods = type.GetMethods();
-                        FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                        foreach (MethodInfo method in methods)
-                        {
-                            if (method.DeclaringType.Name == type.Name)     //прочтем все функции объекта метаданных с нужными именами             
-                            {
+        https://docs.microsoft.com/en-us/dotnet/api/system.type.getfields?view=netframework-4.7.2
 
-                                                                sb.AppendLine("=1===============================================================");
-                                                                sb.AppendLine(String.Format("Method Name: {0}", method.DeclaringType.Name));
+        https://metanit.com/sharp/tutorial/14.2.php
+        https://www.nookery.ru/understand-with-reflection/
 
-                                                                sb.AppendLine("================================================================");
-                                                                sb.AppendLine(String.Format("Method Name: {0}", method.Name));
-                                                                sb.AppendLine("================================================================");
-
-                                                                sb.AppendLine(String.Format("Contains Generic Parameters: {0}", method.ContainsGenericParameters.ToString()));
-                                                                sb.AppendLine(String.Format("Is Abstract?: {0}", method.IsAbstract.ToString()));
-
-                                                               sb.AppendLine(String.Format("Is a Constructor?: {0}", method.IsConstructor.ToString()));
-                                                                sb.AppendLine(String.Format("Is it Generic?: {0}", method.IsGenericMethod.ToString()));
-
-                                                                sb.AppendLine(String.Format("Is it Private?: {0}", method.IsPrivate.ToString()));
-                                                                sb.AppendLine(String.Format("Is it Public?: {0}", method.IsPublic.ToString()));
-                                                                sb.AppendLine(String.Format("Is it Static?: {0}", method.IsStatic.ToString()));
-                                                                sb.AppendLine(String.Format("Is is Virtual?: {0}", method.IsVirtual.ToString()));
-
-                                                                //if the method is a void, the Return type will be null
-                                                                //otherwise, it will return a Type
-                                                                if (method.ReturnType != null && !String.IsNullOrEmpty(method.ReturnType.Name))
-                                                                {
-                                                                    sb.AppendLine(String.Format("Return Type: {0}", method.ReturnType.Name.ToString()));
-                                                                }
-
-                            }
-                            //there are more properties of the MethodInfo you could output...
-                        }
-                        //                        string output = sb.ToString();
-                    }
-
-                    sb.AppendLine("===============================================================");
-                    sb.AppendLine(String.Format("Type Name: {0}", type.Name));
-                    sb.AppendLine("===============================================================");
-
-                    sb.AppendLine(String.Format("Type FullName: {0}", type.FullName));
-                    sb.AppendLine(String.Format("Namespace: {0}", type.Namespace));
-
-                    sb.AppendLine(String.Format("Is it a Class?: {0}", type.IsClass.ToString()));
-                    sb.AppendLine(String.Format("Is it an Interface?: {0}", type.IsInterface.ToString()));
-                    sb.AppendLine(String.Format("Is it Generic?: {0}", type.IsGenericType.ToString()));
-                    sb.AppendLine(String.Format("Is it Public?: {0}", type.IsPublic.ToString()));
-                    sb.AppendLine(String.Format("Is it Sealed?: {0}", type.IsSealed.ToString()));
-
-                    sb.AppendLine(String.Format("Qualified Name: {0}", type.AssemblyQualifiedName));
-
-                    if (type.BaseType != null && !String.IsNullOrEmpty(type.BaseType.Name))
-                    {
-                        sb.AppendLine(String.Format("Base Type: {0}", type.BaseType.Name));
-                    }
-
-                    //there are many, many more properties that an be shown...
-
-                }
-                //                string output = sb.ToString();
-            }
+        https://www.thebestcsharpprogrammerintheworld.com/2017/01/29/using-linq-with-reflection-in-c/
 
 */
